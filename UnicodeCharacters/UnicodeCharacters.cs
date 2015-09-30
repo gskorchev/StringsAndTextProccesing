49cb01e685274ad702ccbb1d73db174f98209832
gskorchev <gskorchev@gmail.com>
2015-09-30 23:30:22 +0300

    homework
    homework

---
+++ UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacters
+{
+    class UnicodeCharacters
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var character in input)
+            {
+                sb.Append("\\u");
+                sb.Append(String.Format("{0:x4}", (int)character)); ;
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
